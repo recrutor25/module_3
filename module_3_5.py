bce08e164013612e0381f4b5df098e4e94e0995f
recrutor25 <sommersi@yandex.ru>
2024-10-14 22:26:28 +0200

    Initial commit

---
+++ module_3_5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def get_multiplied_digits(number):
+
+
+    same_words = []
+    words = list(other_words)
+
+    for i in range(len(words)):
+        if root_word.lower() in words[i].lower() or words[i].lower() in root_word.lower():
+            same_words.append(words[i])
+    return (same_words)
+
