3005a573b262f74c8c9d5e340d34ba239e40cb40
recrutor25 <sommersi@yandex.ru>
2024-10-15 09:58:22 +0200

    Initial commit

--- module_3_5.py
+++ module_3_5.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 def get_multiplied_digits(number):
+    str_number = str(number) # извлекаем строковое представление числа
+    first = int(str_number[0]) # первый символ в числовом представлении
+    if len(str_number) >1 :
+ # умножаем 1 число на результат работы функции (второе число)
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else: # строка закончилась
+        return int(str_number)
+
+result = get_multiplied_digits(40203)
+print(result)
 
 
-    same_words = []
-    words = list(other_words)
-
-    for i in range(len(words)):
-        if root_word.lower() in words[i].lower() or words[i].lower() in root_word.lower():
-            same_words.append(words[i])
-    return (same_words)
 
